43f8f12fc86aaf6eb6ce7add050498e6413ee19b
phiat <phiat99@gmail.com>
2020-03-08 23:11:19 -0400

    update mix.exs for hex publishing

--- mix.exs
+++ mix.exs
@@ -8,9 +8,10 @@ defmodule Alpa.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      maintainers: [" phiat "],
-      licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/phiat/alpa"}
+      package: package(),
+      description: "api wrapper for alpaca.markets",
+      source_url: "https://github.com/phiat/alpa",
+      name: "alpa"
     ]
   end
 
@@ -22,12 +23,21 @@ defmodule Alpa.MixProject do
     ]
   end
 
+  # package build
+  def package do
+    [
+      maintainers: [" phiat "],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/phiat/alpa"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:httpoison, ">= 0.0.0"},
+      {:httpoison, ">= 1.6.2"},
       {:jason, "~> 1.1"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
